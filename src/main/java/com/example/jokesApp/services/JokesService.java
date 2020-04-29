1081ba34f0c2283bdbf524b568d7ea79c69dff89
Maureen Josephine <josephineakello@2030@gmail.com>
2020-04-29 20:37:26 +0300

    create methods for jokes service

---
+++ src/main/java/com/example/jokesApp/services/JokesService.java
@@ -0,0 +1,6 @@
+package com.example.jokesApp.services;
+
+public interface JokesService {
+
+    String getJoke();
+}
--- src/main/java/com/example/jokesApp/services/JokesServiceImpl.java
+++ src/main/java/com/example/jokesApp/services/JokesServiceImpl.java
@@ -1,4 +1,18 @@
 package com.example.jokesApp.services;
 
-public class JokesServiceImpl {
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokesServiceImpl  implements  JokesService{
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokesServiceImpl() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String getJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
 }
