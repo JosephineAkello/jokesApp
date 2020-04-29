bf0cf560e84e56c68475b138b3544ec1922bde33
Maureen Josephine <onyangomaureen95@gmail.com>
2020-04-29 18:18:48 +0300

    create chuck norris class

---
+++ src/main/java/com/example/jokesApp/ChuckNorrisQuotes.java
@@ -0,0 +1,13 @@
+package com.example.jokesApp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChuckNorrisQuotes {
+
+    private List<String> quotes;
+
+    public ChuckNorrisQuotes(){
+        quotes = new ArrayList<>(123);
+    }
+}
